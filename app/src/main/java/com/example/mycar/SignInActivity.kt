1eecf67dd62a34b5921f75beab14b0d2e990f0f8
zalanddmi <zalanddmi@gmail.com>
2023-10-23 13:15:29 +0500

    Добавлен вход

--- app/src/main/java/com/example/mycar/SignInActivity.kt
+++ app/src/main/java/com/example/mycar/SignInActivity.kt
@@ -2,10 +2,54 @@ package com.example.mycar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import com.example.mycar.models.User
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var editTextEmail: EditText
+    private lateinit var editTextPassword: EditText
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
+    private lateinit var users: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        editTextEmail = findViewById(R.id.editTextEmail)
+        editTextPassword = findViewById(R.id.editTextPassword)
+
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseDatabase.getInstance()
+        users = db.getReference("Users")
+    }
+
+    fun onButtonSignInClick(view: View) {
+        signInUser(view)
+    }
+
+    private fun signInUser(view: View) {
+        if (editTextEmail.text.toString().isEmpty()) {
+            Snackbar.make(view, "Введите вашу почту", Snackbar.LENGTH_SHORT).show()
+            return
+        }
+        if (editTextPassword.text.toString().isEmpty()) {
+            Snackbar.make(view, "Введите пароль", Snackbar.LENGTH_SHORT).show()
+            return
+        }
+        auth.signInWithEmailAndPassword(editTextEmail.text.toString(), editTextPassword.text.toString())
+            .addOnSuccessListener {
+                Snackbar.make(view, "Вы вошли!", Snackbar.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                Snackbar.make(view, e.message.toString(), Snackbar.LENGTH_SHORT).show()
+            }
     }
 }
--- app/src/main/java/com/example/mycar/models/User.kt
+++ app/src/main/java/com/example/mycar/models/User.kt
@@ -1,8 +1,3 @@
 package com.example.mycar.models
 
-class User (firstName: String, lastName: String, email: String, password: String) {
-    var firstName: String = firstName
-    var lastName: String = lastName
-    var email: String = email
-    var password: String = password
-}
+data class User (val firstName: String? = null, val lastName: String? = null, val email: String? = null, val password: String? = null)
